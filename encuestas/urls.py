f9e22b15d2e0cf1cd91c67c07e8ea8f29fcb831d
Ivan Kuzel <kuzeliva@ar.ibm.com>
2017-05-17 21:49:46 -0300

    avances

--- encuestas/urls.py
+++ encuestas/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ejemplo: /encuestas/
     url(r'^$', views.index, name='index'),
+    # ejemplo: /encuestas/5/
+    url(r'^(?P<pregunta_id>[0-9]+)/$', views.detail, name='detail'),
+    # ej: /encuestas/5/results/
+    url(r'^(?P<pregunta_id>[0-9]+)/results/$', views.results, name='results'),
+    # ejemplo: /encuestas/5/vote/
+    url(r'^(?P<pregunta_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- encuestas/views.py
+++ encuestas/views.py
@@ -3,8 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pregunta, Opcion
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola, pagina principal.")
+    # return HttpResponse("Hola, pagina principal.")
+    latest_question_list = Pregunta.objects.order_by('-fecha')[:5]
+    template = loader.get_template('encuestas/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, pregunta_id):
+    return HttpResponse("Estás votando en la pregunta %s." % pregunta_id)
+
+def results(request, pregunta_id):
+    response = "Estás mirando los resultados de la pregunta %s."
+    return HttpResponse(response % pregunta_id)
+
+def vote(request, pregunta_id):
+    return HttpResponse("Estás votando en la pregunta %s." % question_id)
